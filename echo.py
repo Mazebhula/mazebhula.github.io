8821350cad86d289dca40d951b3f3ea6f6249173
dedzebujhb024 <dedzebujhb024@student.wethinkcode.co.za>
2025-02-26 00:11:38 +0200

    added page2

---
+++ echo.py
@@ -0,0 +1,4 @@
+a = ["echo who am I","Dzebu Denzhe","Student developer","Don't let that last statement fool you","I'm pretty great at what i do",
+     "Profficient in python", "javascript","java","flask","and postgresql"]
+for item in a:
+    print("denzhedzebu@denzhes-pc ~ % ",item)
